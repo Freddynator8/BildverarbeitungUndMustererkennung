d08d03f18e4c58f7e7fec5368e19a9a180a0b5db
Frederik Wagner <72168283+Freddynator8@users.noreply.github.com>
2023-12-08 16:13:32 +0100

    [A2] Part 1 merge

--- Ass2/bayesian-denoising/denoising.py
+++ Ass2/bayesian-denoising/denoising.py
@@ -20,10 +20,28 @@ def expectaion_maximization(
 
     for it in range(max_iter):
         # TODO: Implement
-        gammas = np.empty((X.shape[0], K))
-        alphas = alphas
-        mus = mus
-        sigmas = sigmas
+        beta = np.zeros((N, K))
+        # perform cholesky decomposition
+        L = np.linalg.cholesky(np.linalg.inv(sigmas))
+        # iterate over all k for computing betas
+        for k in range(K):
+            # calculate beta_k(x_i); used transpose of L_k in norm!!!
+            beta[:, k] = -0.5 * (np.square(np.linalg.norm(np.matmul(X - mus[k], L[k]), axis=1)) + m * np.log(2 * np.pi)) + np.linalg.slogdet(L[k])[1] + np.log(alphas[k])
+        # calculate all logsumexp(beta(x_i))
+        logsumexp = np.max(beta, axis=1) + np.log(np.sum(np.exp(beta - np.max(beta, axis=1)[:, None]), axis=1))
+        # calculate all gammas
+        gammas = np.exp(beta - logsumexp[:, None])
+        # precalc sum of all gamma_k
+        gamma_sum = np.sum(gammas, axis=0)
+        # calculate all alphas
+        alphas = gamma_sum / N
+        # calculate all mus
+        mus = np.divide(np.matmul(gammas.T, X), gamma_sum[:, None])
+        # iterate over all k for computing all sigma_k
+        for k in range(K):
+            diff = X - mus[k]
+            sum_mat = np.matmul(np.multiply(gammas[:, k], diff.T), diff)
+            sigmas[k] = sum_mat / gamma_sum[k] + eps * np.eye(m)
 
         if it % show_each == 0 and plot:
             utils.plot_gmm(X, alphas, mus, sigmas)
@@ -32,19 +50,47 @@ def expectaion_maximization(
 
 
 def denoise(
-    alphas: np.ndarray,
-    mus: np.ndarray,
-    sigmas: np.ndarray,
-    y: np.ndarray,
-    lamda: float,
-    alpha: float = 0.5,
-    max_iter: int = 30,
+        alphas: np.ndarray,
+        mus: np.ndarray,
+        sigmas: np.ndarray,
+        y: np.ndarray,
+        lamda: float,
+        alpha: float = 0.5,
+        max_iter: int = 30,
 ):
     x_est = y.copy()
 
     # TODO: Precompute A, b and implement the loop
+    N = y.shape[0]
+    K = mus.shape[0]
+    m = mus.shape[1]
+    # precomputation
+    A = np.zeros((K, m, m))
+    b = np.zeros((K, m))
+    for k in range(K):
+        A[k] = np.linalg.inv(np.add(lamda * np.identity(m), np.linalg.inv(sigmas[k])))
+        b[k] = np.matmul(np.linalg.inv(sigmas[k]), mus[k])
+
+    L = np.linalg.cholesky(np.linalg.inv(sigmas))
+
     for it in range(max_iter):
-        x_est = x_est
+        # create buffer to determine argmax
+        # resp = np.zeros((N, K))
+        beta = np.zeros((N, K))
+        # calculate responsibilities
+        for k in range(K):
+            beta[:, k] = -0.5 * (
+                        np.square(np.linalg.norm(np.matmul(x_est - mus[k], L[k]), axis=1)) + m * np.log(2 * np.pi)) + \
+                         np.linalg.slogdet(L[k])[1] + np.log(alphas[k])
+            # diff = x_est - mus[k]
+            # exponential = np.exp(-np.sum(np.multiply(np.matmul(diff, np.linalg.inv(sigmas[k]).T), diff), axis=1) / 2)
+            # resp[:, k] = np.log(alphas[k] * np.power((np.power(2 * np.pi, m) * np.linalg.det(sigmas[k])), -0.5) * exponential)
+        # find responsible component for each frame
+        # resp_comp = np.argmax(resp, axis=1)
+        resp_comp = np.argmax(beta, axis=1)
+        # update x_est
+        x_bar = np.sum(np.multiply(A[resp_comp], (lamda * x_est + b[resp_comp])[:, :, None]), axis=1)
+        x_est = alpha * x_est + (1 - alpha) * x_bar
 
     return x_est
 
